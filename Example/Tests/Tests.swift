2a829fec5adf0632b983e9cc7bfffbf7468912ca
keepcalmandcodecodecode <easy.dev@yandex.ru>
2016-01-21 14:24:17 +0300

    test for last keytime

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -13,8 +13,11 @@ class LinearEasingSpec: QuickSpec {
             valuesAndKeyframes = SweetEasing.valuesAndKeytimes(10, to: 20, fps: 30, duration: 2, function: easingFunc)
         }
         describe("all values between 0 and 1"){
-            it("first value is 0"){
+            it("first keytime is 0"){
                 expect(valuesAndKeyframes.keytimes.first) == 0
+            }
+            it("last keytime is 1"){
+                expect(valuesAndKeyframes.keytimes.last) == 1
             }
         }
     }
