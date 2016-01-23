5e2fa50f135203e49281da3364235055725be023
keepcalmandcodecodecode <easy.dev@yandex.ru>
2016-01-23 17:30:13 +0300

    QuartInOut

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -72,6 +72,11 @@ class EasingFuncsSpec: QuickSpec {
                 ["easing":CubicInOutEasing()]
             }
         }
+        describe("QuartInOut easing: all keytimes between 0 and 1"){
+            itBehavesLike("all keytimes between 0 and 1"){
+                ["easing":QuartInOutEasing()]
+            }
+        }
         
         
         
